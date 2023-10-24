37333d1c6a72fe779a0ff2c8e20ea43db47c4aad
svitbar <svitbar1102@gmail.com>
2023-10-24 04:30:55 +0300

    Add error handling.

--- Lab1/src/main/java/org/example/Main.java
+++ Lab1/src/main/java/org/example/Main.java
@@ -26,23 +26,51 @@ public class Main {
         int m;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a: ");
-        a = sc.nextInt();
+        try {
+            System.out.println("Enter a: ");
+            a = sc.nextInt();
 
-        System.out.println("Enter b: ");
-        b = sc.nextInt();
+            System.out.println("Enter b: ");
+            b = sc.nextInt();
 
-        System.out.println("Enter n: ");
-        n = sc.nextInt();
+            System.out.println("Enter n: ");
+            n = sc.nextInt();
 
-        System.out.println("Enter m: ");
-        m = sc.nextInt();
+            System.out.println("Enter m: ");
+            m = sc.nextInt();
 
-        System.out.println("Sum = " + calculateSum(a, b, n, m));
+            if (validate(a, b, n, m)) {
+                System.out.println("Sum = " + calculateSum(a, b, n, m));
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter an integer.");
+        }
+    }
+
+    public static Boolean validate(int a, int b, int n, int m) {
+        try {
+            validateArgs(a, b, n, m);
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public static void validateArgs(int a, int b, int n, int m) {
+        if (a < 0 || b < 0 || n < 0 || m < 0) {
+            throw new IllegalArgumentException("Input should not be less than zero!");
+        } else if (b == 0) {
+            throw new IllegalArgumentException("Error! Division by zero!");
+        }
     }
 
     public static double calculateSum(int a, int b, int n, int m) {
         double S = 0;
+
+        if (a > n || b > m) {
+            return S;
+        }
 
         for (float i = a; i <= n; i++) {
             for (float j = b; j <= m; j++) {
