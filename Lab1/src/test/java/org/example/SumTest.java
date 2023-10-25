27a441eba0a5b53c7c84adedd47c9395342bb2e6
svitbar <svitbar1102@gmail.com>
2023-10-25 03:52:36 +0300

    Add one more test

--- Lab1/src/test/java/org/example/SumTest.java
+++ Lab1/src/test/java/org/example/SumTest.java
@@ -12,4 +12,10 @@ class SumTest {
         assertEquals(0, Sum.calculateSum(6, 3, 4, 1));
         assertEquals(0, Sum.calculateSum(6, 7, 4, 1));
     }
+
+    @Test
+    void rightArgs() {
+        assertEquals(0.7, Sum.calculateSum(2, 1, 3, 3));
+        assertEquals(3.125, Sum.calculateSum(5, 2, 6, 7));
+    }
 }
