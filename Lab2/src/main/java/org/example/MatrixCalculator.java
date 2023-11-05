eac80edbca715e56f4a97c27b10c5e337a458ce4
svitbar <svitbar1102@gmail.com>
2023-11-05 18:44:37 +0200

    Improve class methods: - add new method that generates random matrix; - add new exception.

--- Lab2/src/main/java/org/example/MatrixCalculator.java
+++ Lab2/src/main/java/org/example/MatrixCalculator.java
@@ -1,18 +1,20 @@
 package org.example;
 
+import java.util.Random;
+
 public class MatrixCalculator {
     public static int[][] twoMatrixSum(char[][] A, char[][] B) {
         if (A.length == 0 || B.length == 0) return new int[0][0];
 
         int rows = A.length;
         int col = A[0].length;
-        int [][] C = new int[rows][col];
+        int[][] C = new int[rows][col];
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < col; j++) {
                 int a = Character.digit(A[i][j], 10);
                 int b = Character.digit(B[i][j], 10);
-                C[i][j] = a+ b;
+                C[i][j] = a + b;
             }
         }
 
@@ -20,7 +22,9 @@ public class MatrixCalculator {
     }
 
     public static int maxRowSum(int[][] C) {
-        if (C.length == 0) return 0;
+        if (C.length == 0) {
+            throw new IllegalArgumentException("Cannot calculate a max row sum of an empty matrix.");
+        }
 
         int res = 0;
         int m = C[0].length;
@@ -38,7 +42,7 @@ public class MatrixCalculator {
 
     public static void showMatrix(int[][] matrix) {
         for (int[] row : matrix) {
-            for (int el: row) {
+            for (int el : row) {
                 System.out.print(el + "\t");
             }
             System.out.println();
@@ -48,11 +52,25 @@ public class MatrixCalculator {
 
     public static void showMatrix(char[][] matrix) {
         for (char[] row : matrix) {
-            for (char el: row) {
+            for (char el : row) {
                 System.out.print(el + "\t");
             }
             System.out.println();
         }
         System.out.println();
+    }
+
+    public static char[][] getRandomMatrix(int n, int m) {
+        char[][] matrix = new char[n][m];
+        Random random = new Random();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int number = random.nextInt(10);
+                matrix[i][j] = (char) (number + '0');
+            }
+        }
+
+        return matrix;
     }
 }
