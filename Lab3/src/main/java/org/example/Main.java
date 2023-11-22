e5206914d63de4414523af654f7d024e91d45a8b
svitbar <svitbar1102@gmail.com>
2023-11-22 16:46:53 +0200

    Add basic logic

--- Lab3/src/main/java/org/example/Main.java
+++ Lab3/src/main/java/org/example/Main.java
@@ -6,5 +6,30 @@ public class Main {
     public static void main(String[] args) {
         // C3 = 1202 % 3 = 2 => String
         // C17 = 1202 % 17 = 12 => sort words by specific symbol count
+
+        String text = "hello! how are you?";
+
+        sortBySymbolCount(text, 'o');
+    }
+
+
+    public static void sortBySymbolCount(String text, char c) {
+        String[] words = text.split("\\W+");
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        for (String word: words) {
+            int count = 0;
+
+            for (char symbol: word.toCharArray()) {
+                if (symbol == c) count++;
+            }
+
+            wordCount.put(word, count);
+        }
+
+        wordCount.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEachOrdered(entry -> System.out.println(entry.getValue() + ": " + entry.getKey()));
     }
 }
