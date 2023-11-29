32dc5d95282b638288e7e374b796ab8431e8d37e
svitbar <svitbar1102@gmail.com>
2023-11-29 08:47:49 +0200

    Add basic functionality

---
+++ Lab5/src/main/java/org/example/sorter/TextSorter.java
@@ -0,0 +1,38 @@
+package org.example.sorter;
+
+import org.example.entities.Letter;
+import org.example.entities.Sentence;
+import org.example.entities.Text;
+import org.example.entities.Word;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TextSorter {
+    public static Word[] getWordArray(String text, Letter c) {
+        Text textToSort = new Text(text);
+        Sentence[] sentences = textToSort.getSentences();
+        ArrayList<Word> wordsList = new ArrayList<>();
+
+        for (Sentence sentence : sentences) {
+            Arrays.stream(sentence.getSentence())
+                    .filter(p -> p instanceof Word)
+                    .map(p -> (Word) p)
+                    .forEach(wordsList::add);
+        }
+
+        return sortBySymbolCount(wordsList.toArray(new Word[0]), c);
+    }
+
+    public static Word[] sortBySymbolCount(Word[] words, Letter c) {
+        return Arrays.stream(words)
+                .sorted((w1, w2) -> Integer.compare(getCount(w2, c), getCount(w1, c)))
+                .toArray(Word[]::new);
+    }
+
+    public static int getCount(Word word, Letter c) {
+        return (int) Arrays.stream(word.getWord())
+                .filter(ch -> ch.getSymbol() == c.getSymbol())
+                .count();
+    }
+}
