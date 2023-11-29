bfbe44b969d2099d46a7a287b0e36a42d7a42372
svitbar <svitbar1102@gmail.com>
2023-11-29 08:24:32 +0200

    Add new constructor to Text class

--- Lab5/src/main/java/org/example/entities/Text.java
+++ Lab5/src/main/java/org/example/entities/Text.java
@@ -1,10 +1,28 @@
 package org.example.entities;
 
+import java.util.Arrays;
+
 public class Text {
     private final Sentence[] sentences;
 
     public Text(Sentence[] sentences) {
         this.sentences = sentences;
+    }
+
+    public Text(String text) {
+        if (text.isEmpty()) {
+            throw new IllegalArgumentException("Text should not be empty.");
+        }
+
+        String[] sentArray = text.split("(?<=[.!?])\\s*");
+        System.out.println(Arrays.toString(sentArray));
+        Sentence[] result = new Sentence[sentArray.length];
+
+        for (int i = 0; i < sentArray.length; i++) {
+            result[i] = new Sentence(sentArray[i]);
+        }
+
+        this.sentences = result;
     }
 
     public Sentence[] getSentences() {
