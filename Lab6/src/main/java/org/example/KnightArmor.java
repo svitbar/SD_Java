2b2425c50674fd89c282761c7d62702e3b44a1ae
svitbar <svitbar1102@gmail.com>
2023-12-11 17:13:12 +0200

    Add class with basic functionality

---
+++ Lab6/src/main/java/org/example/KnightArmor.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.example.armor.Armor;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class KnightArmor  {
+    private final ArrayList<Armor> armor = new ArrayList<>();
+
+    public ArrayList<Armor> getArmor() {
+        return armor;
+    }
+
+    public int calculateArmorPrice() {
+        int price = 0;
+
+        for (Armor item: armor) {
+            price += item.getPrice();
+        }
+
+        return price;
+    }
+
+    public ArrayList<Armor> sortArmorByWeight() {
+        ArrayList<Armor> sortedArray = new ArrayList<>(armor);
+        sortedArray.sort(Comparator.comparing(Armor::getWeight));
+
+        return sortedArray;
+    }
+
+    public ArrayList<Armor> findArmorByPrice(int min, int max) {
+        ArrayList<Armor> armorPrice = new ArrayList<>();
+        for (Armor item: armor) {
+            if (item.getPrice() >= min || item.getPrice() <= max) {
+                armorPrice.add(item);
+            }
+        }
+
+        return armorPrice;
+    }
+
+    public void displayArmor(ArrayList<Armor> armorArray) {
+        for (Armor item: armorArray) {
+            System.out.println(item.toString());
+        }
+    }
+}
